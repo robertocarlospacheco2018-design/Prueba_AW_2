fbdab712334adea63b19c284b8753942175527bd
roberto <you@example.com>
2026-07-05 06:58:00 -0500

    Clean: eliminar carpeta duplicada eventos de la raiz

--- eventos/eventos.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EventosController } from './eventos.controller';
-import { EventosService } from './eventos.service';
-
-@Module({
-  controllers: [EventosController],
-  providers: [EventosService],
-})
-export class EventosModule {}
